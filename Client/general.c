5fcf5f273c4be75726501ef8900f7433ee8806f5
tuan <t6e@kali.Test.local>
2023-12-24 06:53:11 -0500

    First observe feature version

--- Client/general.c
+++ Client/general.c
@@ -7,10 +7,13 @@
 #include <pthread.h>
 
 
-int token = 0;
+uint8_t token = 0;
 int updateTimeInterval = 1;
 int getSensordata();
 void* updateSensorData(void *params);
+void observeRequest(coap_context_t *context, coap_session_t *session, const char* observe_uri, uint8_t* token);
+static coap_response_t observeResponseHandle(coap_context_t *ctx, coap_session_t *session,
+    coap_pdu_t *sent, coap_pdu_t *received, const coap_mid_t mid);
 
 struct updateSensorDataParams {
     coap_context_t *ctx;
@@ -25,6 +28,7 @@ int main(void) {
     coap_uri_t uri;
     struct in_addr ip_address = { htonl(0x7f000001) };
     const char *server_uri = "coap://127.0.0.1:5683/example_data"; 
+    const char* observe_uri = "coap://127.0.0.1:5683/example_data";
     pthread_t sensorDataUpdateThreadID;
 
     // Seed for random number generation
@@ -64,26 +68,12 @@ int main(void) {
         return -1;
     }
 
-    // char sensorData[100];
+    observeRequest(ctx, session, observe_uri, &token);
+    coap_register_response_handler(ctx, observeResponseHandle);
+
     // while (1) {
-    //     memset(sensorData, 0, 100);
-    //     sprintf(sensorData, "%d", getSensordata());
-    //     // Create a CoAP PUT request
-        // request = coap_pdu_init(COAP_MESSAGE_CON, COAP_REQUEST_PUT,
-        // coap_new_message_id(session), coap_session_max_pdu_size(session));
-        // coap_add_option(request, COAP_OPTION_URI_PATH, uri.path.length, uri.path.s);
-
-
-    //     // printf("Random value: %d\n", random_value);
-    //     coap_add_data(request, strlen(sensorData), (const uint8_t *)&sensorData);
-
-    //     // Send the PUT request
-    //     coap_send(session, request);
-    //     // Wait for the response
-    //     coap_io_process(ctx, 1000);
-    //     sleep(1);
+    //     coap_io_process(ctx, 0);
     // }
-    sleep(20);
 
     // Clean up
     pthread_cancel(sensorDataUpdateThreadID);
@@ -119,5 +109,70 @@ void* updateSensorData(void *params) {
         coap_send(session, request);
         coap_io_process(ctx, 0);
         sleep(updateTimeInterval);
+    }
+}
+
+void observeRequest(coap_context_t *context, coap_session_t *session, const char* observe_uri, uint8_t* token) {
+     coap_pdu_t *pdu;
+     coap_uri_t uri;
+
+    /* Create the pdu with the appropriate options */
+    pdu = coap_pdu_init( COAP_MESSAGE_CON, COAP_REQUEST_GET, coap_new_message_id(session),
+                        coap_session_max_pdu_size(session));
+    if (!pdu)
+        return NULL;
+
+    /*
+    * Create uniqueness token for this request for handling unsolicited /
+    * delayed responses
+    */
+    token++;
+    if (!coap_add_token(pdu, sizeof(token), (unsigned char*)&token)) {
+        coap_log_debug("cannot add token to request\n");
+        goto error;
+    }
+
+    coap_split_uri(observe_uri, strlen(observe_uri), &uri);
+
+    coap_optlist_t **options = NULL;
+    coap_insert_optlist(&options, coap_new_optlist(COAP_OPTION_OBSERVE, COAP_OBSERVE_ESTABLISH, NULL));
+    coap_insert_optlist(&options, coap_new_optlist(COAP_OPTION_URI_PATH, uri.path.length, uri.path.s));
+
+    /* Add in all the options (after internal sorting) to the pdu */
+    if (!coap_add_optlist_pdu(pdu, options))
+        goto error;
+
+    coap_send(session, pdu);
+    coap_io_process(context, 0);
+
+    return COAP_RESPONSE_OK;
+
+error:
+    coap_delete_pdu(pdu);
+    return NULL;
+}
+
+static coap_response_t observeResponseHandle(coap_context_t *ctx, coap_session_t *session,
+coap_pdu_t *sent, coap_pdu_t *received, const coap_mid_t mid) {
+    char* data;
+    coap_bin_const_t rcv_token = coap_pdu_get_token(received);
+    uint8_t rcv_code = coap_pdu_get_code(received);
+    uint8_t rcv_type = coap_pdu_get_type(received);
+    
+    
+
+    if (rcv_token != token) {
+    /* drop if this was just some message, or send RST in case of notification */
+        if (!sent && (rcv_type == COAP_MESSAGE_CON ||
+                    rcv_type == COAP_MESSAGE_NON)) {
+        /* Cause a CoAP RST to be sent */
+            return COAP_RESPONSE_FAIL;
+        }
+        return COAP_RESPONSE_OK;
+    }
+
+    if (COAP_RESPONSE_CLASS(rcv_code) == 2) {
+        coap_get_data(received, 2, &data);
+        printf("Received data %s\n", data);
     }
 }
